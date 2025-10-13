136832a040cf31e4b4cf59a43990849acd68b8ad
Jose Esteban Ruiz Vertel <jruiz@q10.com>
2025-10-12 23:44:22 -0500

    paralelo

--- cypress/e2e/PruebasGenerales.cy.js
+++ cypress/e2e/PruebasGenerales.cy.js
@@ -1,16 +1,16 @@
-import Componestes from '../pageObject/Componentes/Navbar.js'
+// import Componestes from '../pageObject/Componentes/Navbar.js'
 
-describe('Pruebas login', () =>{
+// describe('Pruebas login', () =>{
 
-    // Inicio de sesion usando el fixture con la institución y los usuarios de esta
-    beforeEach(() => cy.loginGlobalUser('ordenesPago', 0));
+//     // Inicio de sesion usando el fixture con la institución y los usuarios de esta
+//     beforeEach(() => cy.loginGlobalUser('ordenesPago', 0));
 
-    // // Inicio de sesión usando el objeto guardado de manera global (como variable de entorno) en el cypress.config
-    // beforeEach(() => cy.loginGlobalUser('ordenesPago', 1))
+//     // // Inicio de sesión usando el objeto guardado de manera global (como variable de entorno) en el cypress.config
+//     // beforeEach(() => cy.loginGlobalUser('ordenesPago', 1))
     
 
-    it('Pruebas general', () =>{
-        Componestes.irASubseccion('Institucional', 'Estructuración', 'Aulas');
-    })
+//     it('Pruebas general', () =>{
+//         Componestes.irASubseccion('Institucional', 'Estructuración', 'Aulas');
+//     })
 
-})
+// })
